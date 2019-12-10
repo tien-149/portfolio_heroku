358cf1409b3e97e2be97f6dc4469611effcc4a90
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-12-10 15:17:57 -0500

    ready to deploy

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
-    port: 8889, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_portfolio"
+    host: "cleardb.net",
+    // port: 8889, // 8889 for mac
+    uname: "b00cd95475bbac",
+    pword: "1edd6f8b", // leave blank for windows
+    database: "heroku_75774210588b544"
 }
 
 module.exports = config;
